f657f5aa090e19ae3fa892ad1bc1f8d674fa17fe
kiyotd <torigoedesign+kiyotd@gmail.com>
2020-08-04 09:01:30 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useRef} from 'react'
+import lottie from 'lottie-web'
 
 function App() {
+
+  const container = useRef(null)
+
+  let animObj = null
+  let isLiked = false
+
+  const loadAnimation = () => {
+    animObj = lottie.loadAnimation({
+      container: container.current,
+      renderer: 'svg',
+      autoplay: false,
+      loop: false,
+      animationData: require('./animation.json'),
+    })
+    stopAnimation()
+  }
+
+  const handleClick = () => {
+    isLiked ? stopAnimation() : startAnimation()
+    isLiked = !isLiked
+  }
+
+  const startAnimation = () => {
+    animObj.goToAndPlay(0)
+  }
+
+  const stopAnimation = () => {
+    animObj.goToAndStop(1100)
+  }
+
+  useEffect(() => {
+    loadAnimation()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin: '40px'}}>
+      <h1>React Lottie Demo</h1>
+      <p>Click the heart icon to toggle animation</p>
+      <div className="container" ref={container} style={{width: '100px', height: '100px'}} onClick={handleClick}/>
+      <p><a href="https://github.com/kiyotd/react-lottie-like-button"><i className="fa fa-github"/></a></p>
     </div>
-  );
+  )
 }
 
 export default App;
